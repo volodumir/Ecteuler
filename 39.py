ca5fcb7c9e67a7b86474971cfc781415b6a19566
volodumir <volodumir@gmail.com>
2022-03-13 18:15:01 +0200

    Created automatically via Python

---
+++ 39.py
@@ -0,0 +1,7 @@
+import time
+start = time.time()
+
+
+
+stop = time.time()-start
+print(stop)
--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import time
 start = time.time()
 
+txt = '''import time
+start = time.time()
+
+
+
+stop = time.time()-start
+print(stop)'''
+
 a = [i for i in range(4,100)]
 
-print(a)
+for key in a:
+    my_file = open(str(key) + '.py', "w+")
+    my_file.write(txt)
+    my_file.close()
 
 stop = time.time()-start
 
