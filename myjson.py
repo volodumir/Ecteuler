d78e46c0699e5ce46f26a5b663f0abbfcc1b3515
volodumir <volodumir@gmail.com>
2022-03-14 23:16:14 +0200

    Created by Python

--- myjson.py
+++ myjson.py
@@ -1,17 +1,20 @@
 import json
 import urllib.request
 
-# download raw json object
-url ='https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date=20200302&json'
-data = urllib.request.urlopen(url).read().decode()
+date = str('20200302')
 
-# parse json object
-obj = json.loads(data)
+def myjson(date):
+    # download raw json object
+    url ='https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date='+date+'&json'
+    data = urllib.request.urlopen(url).read().decode()
 
-print(obj[0])
+    # parse json object
+    obj = json.loads(data)
 
-print(obj[0].get('rate', 0))
-print(obj[0].get('cc', 0))
-print(obj[0].get('txt', 0))
-print(obj[0].get('r030', 0))
-print(obj[0].get('exchangedate', 0))
+    print(obj[0].get('rate', 0))
+    print(obj[0].get('cc', 0))
+    print(obj[0].get('txt', 0))
+    print(obj[0].get('r030', 0))
+    print(obj[0].get('exchangedate', 0))
+
+myjson(date)
