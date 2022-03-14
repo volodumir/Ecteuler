3e11c777c1ca799782b1408ba24c31599b35fdd1
volodumir <volodumir@gmail.com>
2022-03-14 23:41:34 +0200

    Created by Python

---
+++ mysqlite.py
@@ -0,0 +1,14 @@
+import _sqlite3
+
+def mysqlite(exchangedate, rate):
+    base = _sqlite3.connect('nbu.db')
+    cur = base.cursor()
+    base.execute('CREATE TABLE IF NOT EXISTS {}(exchangedate PRIMARY KEY, rate)'.format('currency'))
+    base.commit()
+
+    cur.execute('INSERT INTO currency VALUES(?, ?)', (exchangedate, rate))
+    base.commit()
+
+    base.close()
+
+mysqlite('29.04.1994', '26.9492')
